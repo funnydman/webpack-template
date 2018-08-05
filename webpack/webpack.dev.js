a6b6e0e52626102ee15cfa4ec75cb0fe250edbb7
FUNNYDMAN <crazy-dzmitry@mail.ru>
2018-08-05 14:28:41 +0300

    add simple configuration

---
+++ webpack/webpack.dev.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const webpack = require('webpack');
+
+module.exports = merge(common, {
+    mode: 'development',
+    devtool: 'inline-source-map',
+});
